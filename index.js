1ecaefa3798d08907a0ca7b9e2bb249cf227041a
Christina <buggsy1106@gmail.com>
2022-03-08 20:33:32 -0600

    commit tree 7bbb22d428e42f10c2d5c41f54089956fc29ade5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(array){
+    let winningYear =array.find((array) => 
+    array.result === 'W');
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        undefined = 'undefined'
+    }
+}
+superbowlWin(record);
 
